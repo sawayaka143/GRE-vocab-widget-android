ca3b3453cf7d00adb96e945305609b3b6993feb8
Sawayaka <eskenoval14@gmail.com>
2026-08-08 02:20:19 +0500

    chore: Update versionCode to 3 and versionName to 1.2 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 33
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
